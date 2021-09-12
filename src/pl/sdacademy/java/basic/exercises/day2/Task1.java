d17e055c56baa2aa50dc52701dbd87552197bac7
Pawel Dudko <paweldudko@outlook.com>
2021-09-12 10:22:09 +0200

    day2 task1

---
+++ src/pl/sdacademy/java/basic/exercises/day2/Task1.java
@@ -0,0 +1,30 @@
+package pl.sdacademy.java.basic.exercises.day2;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        //System.out.println(getLastCharacterFromStringV1(null));
+        System.out.println(getLastCharacterFromStringV2("domek"));
+//        System.out.println(getLastCharacterFromStringV1(" "));
+//        System.out.println(getLastCharacterFromStringV1("     "));
+//        System.out.println(getLastCharacterFromStringV1("   ala  "));
+    }
+
+    private static char getLastCharacterFromStringV1(String input) {
+        if(input == null || input.isBlank()) {
+            System.exit(-1);
+        }
+        input = input.trim();
+        int lastPosition = input.length() - 1;
+        return input.charAt(lastPosition);
+    }
+
+    private static String getLastCharacterFromStringV2(String input) {
+        if(input == null || input.isBlank()) {
+            System.exit(-1);
+        }
+        input = input.trim();
+        int lastPosition = input.length() - 1;
+        return input.substring(lastPosition);
+    }
+}
